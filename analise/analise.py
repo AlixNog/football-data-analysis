e52fa05a184065951ed2734fb22b04e3a51a8c4b
Alice <78050386+AlixNog@users.noreply.github.com>
2025-08-12 11:14:08 -0300

    feat: mais modificacoes

--- analise/analise.py
+++ analise/analise.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
+# Caminhos para a pasta data
+c1 = "data/club_league.csv"
+c2 = "data/clubs.csv"
+c3 = "data/leagues.csv"
+c4 = "data/players.csv"
+c5 = "data/transfers.csv"
 
-c1 = "club_league.csv"
-c2 = "clubs.csv"
-c3 = "leagues.csv"
-c4 = "players.csv"
-c5 = "transfers.csv"
-
-
-d1 = pd.read_csv("club_league.csv", sep=';')
-d2 = pd.read_csv("clubs.csv", sep=';')
-d3 = pd.read_csv("leagues.csv", sep=';')
-d4 = pd.read_csv("players.csv", sep=';')
-d5 = pd.read_csv("transfers.csv", sep=';')
+# Leitura dos arquivos usando os caminhos
+d1 = pd.read_csv(c1, sep=';')
+d2 = pd.read_csv(c2, sep=';')
+d3 = pd.read_csv(c3, sep=';')
+d4 = pd.read_csv(c4, sep=';')
+d5 = pd.read_csv(c5, sep=';')
 
 print(d1.head())
 print(d2.head())
